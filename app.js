4bd31dbc615896916eb3b4abd811616142319216
Abbas Mahmud <ahlbany9@gmail.com>
2024-07-14 23:57:30 +0100

    jul-14, add timeout to alert, made the category page

--- app.js
+++ app.js
@@ -10,20 +10,19 @@ require('./passport/localstrategy')(passport);
 
 const app = express();
 
-// bodyparser Middleware
+// Body parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Connect to MongoDB
 mongoose
   .connect('mongodb://127.0.0.1:27017/registrationDB')
-  .then(() => {
-    console.log('Successfully Connected');
-  })
-  .catch(error => {
-    console.error(`Failed to connect: ${error}`);
+  .then(() => console.log('Successfully Connected'))
+  .catch(err => {
+    console.error(`Failed to connect: ${err}`);
     process.exit(1);
   });
 
+// Express session middleware
 app.use(
   session({
     secret: 'myComplexAndRandomSecretKey123!',
@@ -32,24 +31,22 @@ app.use(
   })
 );
 
-// initialize middleware
+// Passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
 
-// instance of flash
+// Connect flash
 app.use(flash());
 
-// Global variables for flash alert
+// Global variables for flash messages
 app.use((req, res, next) => {
   res.locals.success_msg = req.flash('success_msg');
-  res.locals.success = req.flash('success');
   res.locals.error_msg = req.flash('error_msg');
   res.locals.error = req.flash('error');
-  res.locals.flash = req.flash();
   next();
 });
 
-// serving the static files
+// Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Set EJS as templating engine
@@ -59,18 +56,13 @@ app.set('views', path.join(__dirname, 'views'));
 // Router middleware
 const productRoutes = require('./routes/product.route');
 const adminRoutes = require('./routes/admin.routes');
-const users = require('./routes/user.routes');
-
-// const adminRouter = require('./routes/adminroutes');
-// const cashierRouter = require('./routes/cashierroute');
+const userRoutes = require('./routes/user.routes');
 
 app.use('/users', productRoutes);
 app.use('/', adminRoutes);
-app.use('/users', users);
-// app.use('/admin', adminRouter);
-// app.use('/cashier', cashierRouter);
+app.use('/users', userRoutes);
 
-// Listening
+// Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
